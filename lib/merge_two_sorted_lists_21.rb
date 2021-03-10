f00e74daa9ed67b8285bf616471d4c961799cae6
Zach Stearns <zach.stearns92@gmail.com>
2021-03-09 21:10:53 -0700

    Complete exercise, successful on leetcode site

--- lib/merge_two_sorted_lists_21.rb
+++ lib/merge_two_sorted_lists_21.rb
@@ -1,5 +1,29 @@
 class MergeTwo
   def merge_two(list1, list2)
-    list1.zip(list2).flatten
+    acc = []
+
+    while !list1.nil? && !list2.nil?
+      if list1.val <= list2.val
+        acc.push(list1.val)
+        list1 = list1.next
+      else
+        acc.push(list2.val)
+        list2 = list2.next
+      end
+    end
+
+    if !list1.nil?
+      until list1.nil?
+        acc.push(list1.val)
+        list1 = list1.next
+      end
+    elsif !list2.nil?
+      until list2.nil?
+        acc.push(list2.val)
+        list2 = list2.next
+      end
+    end
+
+    acc
   end
 end
