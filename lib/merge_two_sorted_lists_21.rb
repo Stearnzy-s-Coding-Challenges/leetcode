ecdf3358007a49d77cb3a2155d1ad13e21b8b269
Zach Stearns <zach.stearns92@gmail.com>
2021-03-09 19:42:54 -0700

    Write tests, pass with method, but WIP to rewrite to pass LC case with linked lists

---
+++ lib/merge_two_sorted_lists_21.rb
@@ -0,0 +1,5 @@
+class MergeTwo
+  def merge_two(list1, list2)
+    list1.zip(list2).flatten
+  end
+end
---
+++ test/merge_two_sorted_lists_21_test.rb
@@ -0,0 +1,43 @@
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/merge_two_sorted_lists_21'
+
+class MergeTwoTest < Minitest::Test
+  def test_it_exists
+    merger = MergeTwo.new
+
+    assert_instance_of MergeTwo, merger
+  end
+
+  def test_two_arrays_merge_to_one_array
+    merger = MergeTwo.new
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [2, 3, 5, 6, 7]
+
+    result = merger.merge_two(list1, list2)
+
+    assert_instance_of Array, result
+  end
+
+  def test_result_array_contains_all_elements
+    merger = MergeTwo.new
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [2, 3, 5, 6, 7]
+
+    result = merger.merge_two(list1, list2)
+
+    assert_equal 10, result.count
+  end
+
+  def test_result_array_is_sorted
+    merger = MergeTwo.new
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [2, 3, 5, 6, 7]
+
+    result = merger.merge_two(list1, list2)
+    expected = [1, 2, 2, 3, 3, 4, 5, 5, 6, 7]
+
+    assert_equal expected, result.sort
+  end
+end
