3798719283e8962d9f5f30513a6ba9a22c4e5be4
Zach Stearns <zach.stearns92@gmail.com>
2021-04-05 20:05:53 -0600

    Add tests for sorted and unsorted arrays

--- test/720_longest_word_in_dictionary_test.rb
+++ test/720_longest_word_in_dictionary_test.rb
@@ -9,4 +9,25 @@ class LongestWordInDictionaryTest < Minitest::Test
 
     assert_instance_of LongestWordInDictionary, machine
   end
+
+  def test_it_can_return_word_from_one_element_array
+    machine = LongestWordInDictionary.new
+    words = ['test']
+
+    assert_equal 'test', machine.longest_word(words)
+  end
+
+  def test_it_can_find_biggest_word_from_sorted_array
+    machine = LongestWordInDictionary.new
+    words = ["w","wo","wor","worl", "world"]
+
+    assert_equal 'world', machine.longest_word(words)
+  end
+
+  def test_it_can_find_biggest_word_from_unsorted_array
+    machine = LongestWordInDictionary.new
+    words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+
+    assert_equal 'apple', machine.longest_word(words)
+  end
 end
