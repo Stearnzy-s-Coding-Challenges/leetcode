9ad7f8b4289bbcc3af5d1c66a4e7ec5ff6aacf94
Zach Stearns <zach.stearns92@gmail.com>
2021-04-05 19:22:46 -0600

    Passes leetcode

--- lib/9_palindrome_number.rb
+++ lib/9_palindrome_number.rb
@@ -1,5 +1,18 @@
 class PalindromeNumber
   def is_palindrome(x)
     return false if x.negative?
+
+    x_split = x.to_s.split('')
+
+    while x_split.count > 1
+      accumulator = []
+
+      accumulator << x_split.shift
+      accumulator << x_split.pop
+
+      return false unless accumulator.all?(accumulator[0])
+    end
+
+    true
   end
 end
