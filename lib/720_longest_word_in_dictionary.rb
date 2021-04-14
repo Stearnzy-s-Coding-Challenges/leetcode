002c6a1b75cad2edeb2dddb7f9ecb0a94bcf9469
Zach Stearns <zach.stearns92@gmail.com>
2021-04-13 20:16:39 -0600

    Received help for solution to problem

--- lib/720_longest_word_in_dictionary.rb
+++ lib/720_longest_word_in_dictionary.rb
@@ -1,71 +1,19 @@
 class LongestWordInDictionary
   def longest_word(words)
-    if words.length == 1
-      return words[0]
+    return words[0] if words.length == 1
+
+    words.sort!
+
+    collector = {}
+    answer = ''
+
+    words.each do |word|
+      if word.length == 1 || collector[word[0..-2]]
+        collector[word] = true
+        answer = word if word.length > answer.length
+      end
     end
 
-    # HASH BETTER SUITED FOR THIS PROBLEM!
-
-
-
-    
-
-  #   acc = []
-  #   possible_results =[]
-
-  #   words.sort.each_with_index do |word, index|
-  #     if acc.empty? && word.length == 1
-  #       acc << word
-  #     elsif !acc.empty? && word[0..-2] == acc.last
-  #       acc << word
-  #     elsif !acc.empty? && word[0..-2] != acc.last && word.length == 1
-  #       possible_results << acc.last
-  #       acc = []
-  #       acc << word
-  #     elsif word[0..-2] == acc.last && index == words.length - 1
-  #       possible_results << word
-  #     end
-  #   end
-
-  #   possible_results << acc.last if acc.last[0..-2] == acc[-2]
-
-  #   word_length = possible_results.max_by{|word| word.length}.length
-  #   answer = possible_results.delete_if{|word| word.length < word_length}.sort[0]
-
-  #   return answer
-  # end
-
-
-  # def longest_word(words)
-#     if words.length == 1
-#       return words[0]
-#     end
-
-#     acc = []
-#     possible_results = []
-
-#     words.sort.each_with_index do |word, index|
-#       if acc.empty? && word.length == 1
-#         acc << word
-#       elsif word[0..-2] == acc.last
-#         #  && index == words.length - 1
-#         possible_results << word
-#       elsif word[0..-2] == acc.last
-#         acc << word
-#       elsif word[0..-2] != acc.last
-#         possible_results << acc.last
-#         acc = []
-#       end
-#     end
-
-#     longest_word = possible_results.compact.sort_by do |word|
-#       word.length
-#     end.last
-# require 'pry'; binding.pry
-#     answer = possible_results.find_all do |word|
-#       word.length == longest_word.length
-#     end.sort.first
-
-#     return answer
-#   end
+    answer
+  end
 end
