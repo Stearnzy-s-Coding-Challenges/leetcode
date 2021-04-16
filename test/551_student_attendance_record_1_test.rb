8d9b5d181c5b1bb5ac5d597420626659eb7b8077
Zach Stearns <zach.stearns92@gmail.com>
2021-04-16 16:50:59 -0600

    Add perfect attendance test, one absence and two absences

--- test/551_student_attendance_record_1_test.rb
+++ test/551_student_attendance_record_1_test.rb
@@ -8,4 +8,25 @@ class StudentAttendanceRecordOneTest < Minitest::Test
     record = StudentAttendanceRecordOne.new
     assert_instance_of StudentAttendanceRecordOne, record
   end
+
+  def test_returns_true_if_student_has_no_absences_or_lates
+    record = StudentAttendanceRecordOne.new
+    attendance = 'PPPPP'
+
+    assert record.check_record(attendance)
+  end
+
+  def test_returns_true_if_only_absent_once
+    record = StudentAttendanceRecordOne.new
+    attendance = 'PPPPA'
+
+    assert record.check_record(attendance)
+  end
+
+  def test_returns_false_if_absent_twice
+    record = StudentAttendanceRecordOne.new
+    attendance = 'APPPA'
+
+    assert_equal false, record.check_record(attendance)
+  end
 end
