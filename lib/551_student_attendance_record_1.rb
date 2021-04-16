83e8f81352111559c4e3075b17b0e8cc0a8686bf
Zach Stearns <zach.stearns92@gmail.com>
2021-04-16 16:51:13 -0600

    Absence check added

--- lib/551_student_attendance_record_1.rb
+++ lib/551_student_attendance_record_1.rb
@@ -1,3 +1,17 @@
 class StudentAttendanceRecordOne
-  
+  #?  Return false if string has 2 or more Absences
+  #?  Return false if string has 3 or more CONSECUTIVE Lates
+
+  def check_record(attendance)
+    record = attendance.split('')
+
+    absent_record = 0
+    record.each_with_index do |presence, index|
+      absent_record += 1 if presence == 'A'
+
+      return false if absent_record == 2
+    end
+
+    true
+  end
 end
