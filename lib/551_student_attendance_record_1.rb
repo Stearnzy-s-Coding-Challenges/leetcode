2e67d5eb9e5a5a412beda63ed085373080c3e521
Zach Stearns <zach.stearns92@gmail.com>
2021-04-16 17:11:01 -0600

    Complete method and passes tests

--- lib/551_student_attendance_record_1.rb
+++ lib/551_student_attendance_record_1.rb
@@ -1,15 +1,16 @@
 class StudentAttendanceRecordOne
-  #?  Return false if string has 2 or more Absences
-  #?  Return false if string has 3 or more CONSECUTIVE Lates
-
   def check_record(attendance)
     record = attendance.split('')
 
     absent_record = 0
+
     record.each_with_index do |presence, index|
       absent_record += 1 if presence == 'A'
-
       return false if absent_record == 2
+
+      if index >= 2 && presence == 'L' && record[index - 1] == 'L' && record[index - 2] == 'L'
+        return false
+      end
     end
 
     true
