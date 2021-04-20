d31be9a9c14103222f676303d196dd87c3d56980
Zach Stearns <zach.stearns92@gmail.com>
2021-04-20 14:05:09 -0600

    complete find vowels method

--- lib/345_reverse_vowels_of_string.rb
+++ lib/345_reverse_vowels_of_string.rb
@@ -1,3 +1,10 @@
 class ReverseVowels
-  
+  def find_vowels(string)
+    all_vowels = ['a', 'e', 'i', 'o', 'u']
+    letters = string.split('')
+
+    letters.select do |letter|
+      all_vowels.include?(letter)
+    end
+  end
 end
