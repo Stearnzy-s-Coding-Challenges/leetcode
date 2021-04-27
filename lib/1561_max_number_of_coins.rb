f47392c3b5dbbb61cf7d3643c39425b48f6816ed
Zach Stearns <zach.stearns92@gmail.com>
2021-04-27 15:59:27 -0600

    Complete exercise, runs slowly

--- lib/1561_max_number_of_coins.rb
+++ lib/1561_max_number_of_coins.rb
@@ -1,3 +1,22 @@
 class MaxCoins
-  
+  def max_coins(piles)
+    #? Want Bob to get 3 lowest at all times
+    #? All piles divisible by 3 == Omit lowest 3
+    #? I get every second set
+
+    piles.sort!.reverse!
+    turns = piles.count / 3
+
+    turns.times do
+      piles.pop
+    end
+
+    count = 0
+
+    piles.each_with_index do |coins, index|
+      count += coins if index.odd?
+    end
+
+    count
+  end
 end
