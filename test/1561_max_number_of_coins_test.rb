faffc85d814c846b4959f7315821b83155256763
Zach Stearns <zach.stearns92@gmail.com>
2021-04-27 15:52:21 -0600

    Add given tests

--- test/1561_max_number_of_coins_test.rb
+++ test/1561_max_number_of_coins_test.rb
@@ -8,4 +8,25 @@ class MaxCoinsTest < Minitest::Test
     counter = MaxCoins.new
     assert_instance_of MaxCoins, counter
   end
+
+  def test_pile_of_three
+    counter = MaxCoins.new
+    piles = [2, 4, 5]
+
+    assert_equal 4, counter.max_coins(piles)
+  end
+
+  def test_pile_of_six
+    counter = MaxCoins.new
+    piles = [2, 4, 1, 2, 7, 8]
+
+    assert_equal 9, counter.max_coins(piles)
+  end
+
+  def test_pile_of_three
+    counter = MaxCoins.new
+    piles = [9, 8, 7, 6, 5, 1, 2, 3, 4]
+
+    assert_equal 18, counter.max_coins(piles)
+  end
 end
